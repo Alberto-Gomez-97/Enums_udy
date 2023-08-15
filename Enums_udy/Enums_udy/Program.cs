82a2e3b9862e4ee57f1f1b1e96455b23b93a61be
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-15 13:18:07 -0600

    Add files via upload

---
+++ Enums_udy/Enums_udy/Program.cs
@@ -0,0 +1,33 @@
+﻿//ENUMS
+//agrupar conjunto de constantes numericas
+
+using Enums_udy;
+
+var esttatusDeVentaDeUnaCamisa = EstatusVenta.PendienteDePago;
+
+//accion del estatus
+//en switch presiona ctrl+. para colocar todos los posibles estatus de la clase
+switch (esttatusDeVentaDeUnaCamisa)
+{
+    case EstatusVenta.Exitoso:
+        Console.WriteLine("La venta fue exitosa");
+        break;
+    case EstatusVenta.PendienteDePago:
+        Console.WriteLine("El cliente debe pagar");
+        break;
+    case EstatusVenta.Cancelada:
+        Console.WriteLine("Nooooooooo :(");
+        break;
+    default:
+        Console.WriteLine("Estatus desconocido");
+        break;
+}
+
+//ejemplo 2 con casteos
+
+int estatus = (int)esttatusDeVentaDeUnaCamisa;
+Console.WriteLine(estatus);//1
+
+int estatus2= 3;
+var estatus2Nombre = (EstatusVenta)estatus2;
+Console.WriteLine(estatus2Nombre);
